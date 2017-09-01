6a6d6750e1164626eb126cf42b36d4d2db7e444c
AbdallahBeraida <31482325+AbdallahBeraida@users.noreply.github.com>
2017-09-01 16:24:21 +0100

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
        		<meta charset='utf-8'>
 		<meta name="author" content="AbdallahBeraida" />
