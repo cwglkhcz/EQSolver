b3e8ffb525db3c27fa93b9adaf5c7b5175cd7e19
AbdallahBeraida <31482325+AbdallahBeraida@users.noreply.github.com>
2017-09-01 16:12:21 +0100

    Update second_degree.php

--- EQ/second_degree.php
+++ EQ/second_degree.php
@@ -8,7 +8,8 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-        <meta charset='utf-8'>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+        	<meta charset='utf-8'>
 		<meta name="author" content="AbdallahBeraida" />
 		<title>Equation de seconde degree</title>
 		<link rel="stylesheet" type="text/css" href="../style/main.css"/>
